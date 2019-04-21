12e7f6d3073d0372c616d579674427cb3f347919
Brendan Wilson <flubbid@Brendans-MacBook-Pro.local>
2019-04-21 11:19:30 -0500

    created more game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,23 @@
+console.log("Up and running!")
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
+function checkForMatch(){
+	if (cardsInPlay[0] === cardsInPlay[1]) { 
+	console.log("You found a match!");
+} else {
+		console.log("Sorry, try again.");
+	}
+}
+
+function flipCard(cardId){
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId])
+checkForMatch();
+
+}
+flipCard(0);
+flipCard(2);
+
+	
+
